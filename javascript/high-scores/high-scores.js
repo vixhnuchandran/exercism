803291c611a334ba258f12a7f34f1ba555e9d4ef
vixhnuchandran <vixhnuchandran@gmail.com>
2023-09-28 05:12:19 +0530

    ☑

---
+++ javascript/high-scores/high-scores.js
@@ -0,0 +1,27 @@
+//
+// This is only a SKELETON file for the 'High-Scores' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class HighScores {
+  constructor(scoreList) {
+    this.scoreList = scoreList;
+  }
+
+  get scores() {
+    return this.scoreList;
+  }
+
+  get latest() {
+    return this.scoreList[this.scoreList.length - 1];
+  }
+
+  get personalBest() {
+    return Math.max(...this.scoreList);
+  }
+
+  get personalTopThree() {
+    return this.scoreList.sort((a, b) => b - a).slice(0, 3);
+
+  }
+}
