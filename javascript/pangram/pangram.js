a954a5c6d49fd2dc0f1670a46a78a9ba23cc7a1e
vixhnuchandran <vixhnuchandran@gmail.com>
2023-09-29 20:03:51 +0530

    Pangram  #completed✔ on 29/09/2023

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+
+export const isPangram = (input = "") => {
+  const alphas = "abcdefghijklmnopqrstuvwxyz".split("");
+  return alphas.every(each => input.toLowerCase().includes(each));
+};
