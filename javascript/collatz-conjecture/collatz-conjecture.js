c1f3138a61b307a9b7b6616c0a50756310e2ca5c
vixhnuchandran <vixhnuchandran@gmail.com>
2023-10-04 06:01:45 +0530

    Collatz-Conjecture(JS)  ✔Completed on 04/10/2023

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,21 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = (num) => {
-  if (num === 1) return 0;
-  if (num <= 0) {
-    throw new Error('Only positive numbers are allowed');
-  };
-  let count = 0;
-  
-  while (num !== 1) {
-    if (num % 2 === 0) {
-      num = num / 2;
-    }
-    else {
-      num = (3 * num) + 1;
-    }
-    count ++;
-  }
+export const steps = (num, count = 0) => {
+  if (num <= 0) throw new Error('Only positive numbers are allowed');
+  for (; num > 1; count++) num = num % 2 === 0 ? num / 2 : 3 * num + 1;
   return count;
 };
