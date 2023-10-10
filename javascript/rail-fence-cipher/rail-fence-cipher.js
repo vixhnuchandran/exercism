91aba6ebd9369d1b3ce21c877595f7bb401649b6
vixhnuchandran <vixhnuchandran@gmail.com>
2023-10-10 22:30:38 +0530

    Rail-Fence-Cipher(JS) ✔Completed on 10/10/2023

--- javascript/rail-fence-cipher/rail-fence-cipher.js
+++ javascript/rail-fence-cipher/rail-fence-cipher.js
@@ -3,10 +3,49 @@
 // convenience to get you started writing code faster.
 //
 
-export const encode = () => {
-  throw new Error('Remove this statement and implement this function');
+const zigzag = (row, col, rows) => col + 2 * (rows - 1 - row);
+
+export const encode = (plainText, rows) => {
+    if (rows <= 1) return plainText; // No need to encode
+
+    const columns = plainText.length;
+    let encodedText = '';
+
+    for (let r = 0; r < rows; r++) {
+        for (let i = r; i < columns; i += 2 * (rows - 1)) {
+            encodedText += plainText[i];
+
+            if (r > 0 && r < rows - 1) {
+                const zigzagIndex = zigzag(r, i, rows);
+                if (zigzagIndex < columns) {
+                    encodedText += plainText[zigzagIndex];
+                }
+            }
+        }
+    }
+
+    return encodedText;
 };
 
-export const decode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const decode = (cipherText, rows) => {
+    if (rows <= 1) return cipherText;
+    const columns = cipherText.length;
+    const decodedText = Array(columns).fill('');
+
+    let cipherIndex = 0;
+
+    for (let r = 0; r < rows; r++) {
+        for (let i = r; i < columns; i += 2 * (rows - 1)) {
+            decodedText[i] = cipherText[cipherIndex++];
+
+            if (r > 0 && r < rows - 1) {
+                const zigzagIndex = zigzag(r, i, rows);
+                if (zigzagIndex < columns) {
+                    decodedText[zigzagIndex] = cipherText[cipherIndex++];
+                }
+            }
+        }
+    }
+
+    return decodedText.join('');
 };
--- javascript/rail-fence-cipher/rail-fence-cipher.spec.js
+++ javascript/rail-fence-cipher/rail-fence-cipher.spec.js
@@ -6,25 +6,25 @@ describe('Rail Fence Cipher', () => {
       const fence = encode('XOXOXOXOXOXOXOXOXO', 2);
       expect(fence).toEqual('XXXXXXXXXOOOOOOOOO');
     });
-    xtest('encode with three rails', () => {
+    test('encode with three rails', () => {
       const fence = encode('WEAREDISCOVEREDFLEEATONCE', 3);
       expect(fence).toEqual('WECRLTEERDSOEEFEAOCAIVDEN');
     });
-    xtest('encode with ending in the middle', () => {
+    test('encode with ending in the middle', () => {
       const fence = encode('EXERCISES', 4);
       expect(fence).toEqual('ESXIEECSR');
     });
   });
   describe('decode', () => {
-    xtest('decode with three rails', () => {
+    test('decode with three rails', () => {
       const fence = decode('TEITELHDVLSNHDTISEIIEA', 3);
       expect(fence).toEqual('THEDEVILISINTHEDETAILS');
     });
-    xtest('decode with five rails', () => {
+    test('decode with five rails', () => {
       const fence = decode('EIEXMSMESAORIWSCE', 5);
       expect(fence).toEqual('EXERCISMISAWESOME');
     });
-    xtest('decode with six rails', () => {
+    test('decode with six rails', () => {
       const encodedString =
         '133714114238148966225439541018335470986172518171757571896261';
       const fence = decode(encodedString, 6);
