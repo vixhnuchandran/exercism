58a6b50b2f4f545624324b5bc115c5db41940203
vixhnuchandran <vixhnuchandran@gmail.com>
2023-10-14 01:00:27 +0530

    Run-Length-Encoding(JS) ✔Completed on 4/10/2023

---
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -0,0 +1,7 @@
+//
+// This is only a SKELETON file for the 'Run Length Encoding' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const encode = text => text.replace(/(\w|\s)\1+/g, (subStr, char) => `${subStr.length}${char}`);
+export const decode = text => text.replace(/(\d+)(\w|\s)/g, (_, char, c) => c.repeat(char));
